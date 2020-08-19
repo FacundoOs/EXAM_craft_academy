1980a5ffa578606780da4d92eb8e3bfd0998e9b3
Facundo Osores <facundo@MBPdeFacundo2.lan>
2020-08-19 13:35:01 +0200

    More tests added and directory to app.js, modified

--- cypress/integration/buttonCheck.feature.js
+++ cypress/integration/buttonCheck.feature.js
@@ -1,5 +1,10 @@
 describe('Checking Greeting message', () => {
-	it('Message after press the button', () => {
-		cy.visit('http://localhost:3001')
-	})
+	it('Testing connection and functionality', () => {
+        cy.visit('http://localhost:3001')
+        cy.contains('Greetings')
+        cy.get('#quote').click()
+        })
+        it('Message after pressing the button', () => {
+                cy.get('#message').should('contain', 'hello how are you')
+            })
 })
